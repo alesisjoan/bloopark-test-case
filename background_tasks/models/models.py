550971e2b069a8da8a5dfe0f3da8ae2693985425
Alesis Manzano <amanzano@timetrade.com>
2020-11-01 17:24:21 -0300

    Send chats message when task starts and finishes closes #6

--- background_tasks/models/models.py
+++ background_tasks/models/models.py
@@ -2,6 +2,29 @@
 
 from odoo import api, models, _
 
+
+class Chat(models.TransientModel):
+    _name = 'background_tasks.chat'
+
+    @api.model
+    def send_message(self, user_to_ids, message):
+        for user_to in user_to_ids:
+            user = self.env['res.users'].browse(user_to)
+            partner = user.partner_id
+            odoobot_id = self.env['ir.model.data'].xmlid_to_res_id("base.partner_root")
+            channel = self.env['mail.channel'].sudo().search([('uuid', '=', user_to)], limit=1) \
+                .with_context(mail_create_nosubscribe=True).create({
+                'channel_partner_ids': [(4, partner.id), (4, odoobot_id)],
+                'public': 'private',
+                'channel_type': 'chat',
+                'email_send': False,
+                'name': 'OdooBot'
+            })
+            channel.sudo().message_post(body=message, author_id=odoobot_id, message_type="comment",
+                                        subtype="mail.mt_comment")
+        return True
+
+
 class Import(models.TransientModel):
     _name = 'base_import.import'
     _inherit = 'base_import.import'
@@ -10,7 +33,10 @@ class Import(models.TransientModel):
     @api.model
     def do_legacy(self, id, fields, columns, options, dryrun):
         result = super(Import, self.browse(id)).do(fields, columns, options, dryrun)
-
+        model_name = self.env[self.browse(id).res_model]._name
+        self.env['background_tasks.chat'].send_message([self.env.user.id],
+                                                       "Import task for model {} has been finished. Result: {}"
+                                                       .format(model_name, result))
 
     @api.multi
     def do(self, fields, columns, options, dryrun=False):
@@ -28,6 +54,11 @@ class Import(models.TransientModel):
         })
 
         model_name = self.env[self.res_model]._name
+        self.env['background_tasks.chat'].send_message([self.env.user.id],
+                                                       "Import task for model {} has been started."
+                                                       " You will be notified by chat when its finished."
+                                                       .format(model_name))
 
         return "Import task for model {} has been started." \
+               " You will be notified by chat when its finished." \
             .format(model_name)
