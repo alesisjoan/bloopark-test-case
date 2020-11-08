82cd901bf612dfd4b7058c9e4498b6c80d498ee9
alesisjoan <alesis4ta@gmail.com>
2020-11-08 18:13:38 -0300

    Load default configurations, fixes #31

--- background_tasks/__init__.py
+++ background_tasks/__init__.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from . import models
+from odoo import api, SUPERUSER_ID
+
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['ir.config_parameter'].set_param('background_tasks.module_send_chat_start', True)
+    env['ir.config_parameter'].set_param('background_tasks.module_send_chat_finish', True)
+    env['ir.config_parameter'].set_param('background_tasks.module_message_systray', True)
